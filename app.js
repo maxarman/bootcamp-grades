8d99777cb2298ba373b3feb81e25b6cbdb7108ed
maximiliano.arman@wundermanthompson.com <maximiliano.arman@wundermanthompson.com>
2020-09-05 15:12:13 +0200

    Endereco para o cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://desafio-grades-app-igti.herokuapp.com/',
   })
 );
 
